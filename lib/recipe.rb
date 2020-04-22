75b471e47a97bf28961acbc886b59a07d8f541ec
Melanie Tran <melaniexx02@gmail.com>
2020-04-22 09:54:59 -0600

    Created total calories method and test

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -13,4 +13,10 @@ class Recipe
   def ingredients
     @ingredients_required.keys
   end
+
+  def total_calories
+    @ingredients_required.sum do |ingredient, quantity|
+      ingredient.calories * quantity
+    end
+  end
 end
