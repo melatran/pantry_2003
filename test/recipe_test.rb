51de3b4f75a9950822783c07ad80fd4e0fb9a689
Melanie Tran <melaniexx02@gmail.com>
2020-04-22 09:39:35 -0600

    created test and method for listing all ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -23,9 +23,17 @@ class PantryTest < Minitest::Test
     @recipe1.add_ingredient(@ingredient1, 2)
     @recipe1.add_ingredient(@ingredient1, 4)
     @recipe1.add_ingredient(@ingredient2, 8)
-    
+
     expected = {@ingredient1 => 6, @ingredient2 => 8}
 
     assert_equal expected, @recipe1.ingredients_required
+  end
+
+  def test_it_can_list_all_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    
+    assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
   end
 end
