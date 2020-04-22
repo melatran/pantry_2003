0af540a3afa2a16a3e13d1c2c38730b908eb5ca8
Melanie Tran <melaniexx02@gmail.com>
2020-04-22 11:11:26 -0600

    Final commits

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -30,4 +30,22 @@ class CookBook
     Date.today.strftime("%m-%d-%Y")
   end
 
+  def sort_ingredient_name_and_amount(recipe) #new ingredient hash
+    recipe.ingredients_required.map do |ingredient, quantity|
+      details = Hash.new
+      details[:ingredeint] = ingredient.name
+      details[:amount] = quantity.to_s + " " + ingredient.unit
+      details
+    end
+  end
+
+  def summary
+    @recipes.map do |recipe|
+      summary = Hash.new
+      summary[:name] = recipe.name
+      summary[:details][:ingredients] = sort_ingredient_name_and_amount(recipe)
+      summary[:details][:total_calories] = recipe.total_calories
+      summary
+    end
+  end
 end
