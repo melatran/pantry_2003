3938b88a3b276bb216b2e50d02f913ae4826324f
Melanie Tran <melaniexx02@gmail.com>
2020-04-22 09:29:13 -0600

    created stock and restock item test and methods

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient
   attr_reader :name, :unit, :calories
-  
+
   def initialize(ingredient_info)
     @name = ingredient_info[:name]
     @unit = ingredient_info[:unit]
---
+++ test/pantry_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_it_has_no_stock
+    assert_equal ({}), @pantry.stock
+  end
+
+  def test_it_can_check_stock
+    assert_equal 0, @pantry.stock_check(@ingredient1)
+  end
+
+  def test_it_can_restock_item
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal 15, @pantry.stock_check(@ingredient1)
+
+    @pantry.restock(@ingredient2, 7)
+    assert_equal 7, @pantry.stock_check(@ingredient2)
+  end
+end
